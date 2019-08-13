e09919d97da2aa68ac02285cead9925104105618
MarconeAugusto <marcone.p.louzada@gmail.com>
2019-08-13 19:23:03 -0300

    correção

--- sensorAnalog/sensorAnalog.ino
+++ sensorAnalog/sensorAnalog.ino
@@ -29,42 +29,37 @@
 #include <math.h>
 
 float sensorPin = A0 ;    // selecione o pino de entrada para o potenciômetro
-float ledPin = 13 ;      // selecione o pino para o LED
+float ledPin = 22 ;      // selecione o pino para o LED
 float sensorValue = 0 ;  // variável para armazenar o valor vindo do sensor
-float temperatura, A, B, C, Vsensor, Vout, Rsensor;
+float temperatura, A, B, C, Vs, Vo, Rs, ln, ln3;
 
 void setup ( ) {
   // declara o ledPin como um OUTPUT:
   pinMode ( ledPin , OUTPUT );
   Serial.begin(9600); 
-  A = 1.112272e-03;
-  B = 2.375809e-04;
-  C = 0.685285e-07;
-  
+  A = 1.1122e-03;
+  B = 2.3758e-04;
+  C = 0.6852e-07;
 }
 
 void loop ( ) {
+   // liga o ledPin em
+  digitalWrite ( ledPin , LOW ) ;
   // lê o valor do sensor:
   sensorValue = analogRead (sensorPin);
-  Vsensor = sensorValue*5/1023;
-  Vout = 5 - Vs;
-  Rsensor = ((50000/Vout)-10000);
-  Serial.print("Resistência ");
-  Serial.println(Rs);
-  
-  temperatura = 1/(A + B*log(Rs)+ C*pow((log(Rs)),3)); 
-  // liga o ledPin em
-  Serial.print("Sensor ");
-  Serial.println(sensorValue);
+  // converte para a tensão real
+  Vs = sensorValue*5/1023;
+  // calcula o valor da resistencia
+  Rs = ((50000/Vs)-10000);
+  // calcula o valor do ln
+  ln = log(Rs);
+  // calcula o valor da temperatura
+  temperatura = 1/(A + (B*ln)+ C*pow(ln,3))-273.15; 
+  // desliga o ledPin:
+  digitalWrite ( ledPin , HIGH ) ;
+  // imprime via serial o valor da temperatura
   Serial.print("Temperatura ");
   Serial.println(temperatura);
-  delay(1000);
-  //digitalWrite ( ledPin , HIGH ) ;
-  // pare o programa por <sensorValue> milissegundos:
-  //delay ( sensorValue ) ;
-  // desliga o ledPin:
-  //digitalWrite ( ledPin , LOW ) ;
-  // pare o programa por <sensorValue> milissegundos:
-  //delay( sensorValue ) ;
+  // parar o programa por um segundo
+  delay(5000);
 }
-
