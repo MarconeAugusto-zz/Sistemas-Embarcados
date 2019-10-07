b75a92152db8a4b4c7259d2294b2c66f47134b01
GuilhermeRoque <roqueee007@gmail.com>
2019-10-07 08:56:11 -0300

    GPIO_v6

--- MPLABXProject/GPIO_v6.X/main.cpp
+++ MPLABXProject/GPIO_v6.X/main.cpp
@@ -7,14 +7,17 @@
 #include "GPIO.h"
 #include "GPIO_Port.h"
 #include "uart.h"
+#include <stdio.h>
+#include <stdlib.h>
 
  
 GPIO_PORT::GPIO_Port * _Px = GPIO_PORT::AllPorts[9];
 GPIO botao(54, GPIO::INPUT);		//PF0 
 Timer t = Timer(1000);
 uart serial(9600, uart::DATABITS_8, uart::NONE, uart::STOPBIT_1);
+const char msg[] = "O valor atual da porta PK e: ";
 
-uint8_t px_value = 0;
+uint8_t px_value = 1;
 
 void timeout_handler(void){
     _Px->write_byte(px_value);
@@ -28,14 +31,23 @@ void timeout_handler(void){
 
 void check_botao(){
     if(botao.get()){
-        char msg[] = "O valor atual da porta PK é: ";
         serial.puts(msg);
+        /*
+        char valor[5];
+        sprintf(valor,"%d\n",px_value);         
+         */
+        char valor[5];
+        itoa(px_value,valor,10);
+        valor[3]='\n';
+        valor[4]='\0';        
+        serial.puts(valor);
     }
 }
  
 int main(void){
     _Px->dir_byte(1);
-	t.addTimeout(2000, &timeout_handler);     
+	t.addTimeout(2000, &timeout_handler); 
+	t.addTimeout(200, &check_botao);     
     sei();
 
     for(;;){
