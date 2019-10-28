f595cf597a1e5a3b533026836a5c9d1c3e82c866
Marcone <marcone.p.louzada@gmail.com>
2019-10-28 16:20:17 -0300

    Finalizando escrita e leitura na EEPROM
    Signed-off-by: Marcone <marcone.p.louzada@gmail.com>

--- MPLABXProject/I2C.X/main.cpp
+++ MPLABXProject/I2C.X/main.cpp
@@ -14,44 +14,105 @@
 
 #define Slave_Write_Address		0x20
 #define Slave_Read_Address		0x21
-#define	count					10
 
+//#define F_CPU 8000000UL		/* Define CPU clock Frequency 8MHz */
 
-int main(int argc, char** argv) { 
+#define EEPROM_Write_Addess		0xA0
+#define EEPROM_Read_Addess		0xA1
+
+int main(int argc, char** argv){
     UART serial(19200, UART::EIGHT_DB, UART::NONE_PAR, UART::ONE_SB, UART::DS_DISABLE);
-	char info;
-	sei();
-    serial.puts("Iniciando Master \n");
-	TWI twi;								/* Initialize I2C */
-	
-	while(1){
-        serial.puts("Enviando: ");
-		twi.Start_Wait(Slave_Write_Address);/* Start I2C communication with SLA+W */
-		_delay_ms(5);
-		for (uint8_t i = 0; i < count ; i++){
-            serial.put(i);
-			twi.Write(i);					/* Send Incrementing count */
-			_delay_ms(500);
-		}
-		serial.puts("Recebendo: ");
-		twi.Repeated_Start(Slave_Read_Address);	/* Repeated Start I2C communication with SLA+R */
-		_delay_ms(5);
-		for (uint8_t i = 0; i < count; i++){
-			if(i < count - 1){
-				/* Read and send Acknowledge of data */
-                info = twi.Read_Ack();
-                serial.put(info);
-            }else{
-                /* Read and Not Acknowledge to data */
-                info = twi.Read_Nack();
-                serial.put(info);
-            }
-			_delay_ms(500);
-		}
-		twi.Stop();							/* Stop I2C */
-	}
+    char array[8] = "EEPROM";                /* Declaração do array usado no teste */
+    sei();
+    while(1){
+        serial.puts("\n");
+        serial.puts("\n");
+        serial.puts("\n");                      /* Tirando o lixo da serial */
+        serial.puts("\n");
+        _delay_ms(1000);
+        serial.puts("Iniciando...");
+        _delay_ms(100);
+        TWI twi;                                /* Inicializando I2C */
+        serial.puts("\n");
+        twi.Start(EEPROM_Write_Addess);         /* Inicia o I2C com o endereço de gravação do dispositivo */
+        twi.Write(0x00);                        /* Endereço de memória inicial para gravação de dados */
+        serial.puts("Iniciando escrita... ");
+        _delay_ms(100);
+        serial.puts("\n");
+        serial.puts("Escrevendo: ");
+        _delay_ms(100);
+        for (int i = 0; i<strlen(array); i++){  /* Escrevendo array de dados */
+            twi.Write(array[i]); 
+            serial.put(array[i]);
+            _delay_ms(500);
+        }
+        serial.puts("\n");
+        twi.Stop();                             /* Parando I2C */
+        _delay_ms(100);
+        serial.puts("Encerrando escrita...");
+        serial.puts("\n");
+        _delay_ms(100);
+        twi.Start(EEPROM_Write_Addess);         /* Inicia o I2C com o endereço de gravação do dispositivo */
+        twi.Write(0x00);                        /* Escreva o endereço da memória inicial */
+        twi.Repeated_Start(EEPROM_Read_Addess); /* Repeat start I2C SLA+R */
+        serial.puts("Iniciando leitura... ");
+        _delay_ms(100);
+        serial.puts("\n");
+        serial.puts("Lendo: ");
+        _delay_ms(100);
+        for (int i = 0; i<strlen(array); i++){  /* Lendo os dados com ack */
+            serial.put(twi.Read_Ack());
+            _delay_ms(500);
+        }
+        twi.Read_Nack();                        /* Lendo os dados com nack */
+        serial.puts("\n");
+        serial.puts("Encerrando...");
+        _delay_ms(100);
+        twi.Stop();                             /* Parando I2C */
+    }
     return 0;
 }
+
+
+
+//#define	count					10
+//
+//
+//int main(int argc, char** argv) { 
+//    UART serial(19200, UART::EIGHT_DB, UART::NONE_PAR, UART::ONE_SB, UART::DS_DISABLE);
+//	char info;
+//	sei();
+//    serial.puts("Iniciando Master \n");
+//	TWI twi;								/* Initialize I2C */
+//	
+//	while(1){
+//        serial.puts("Enviando: ");
+//		twi.Start_Wait(Slave_Write_Address);/* Start I2C communication with SLA+W */
+//		_delay_ms(5);
+//		for (uint8_t i = 0; i < count ; i++){
+//            serial.put(i);
+//			twi.Write(i);					/* Send Incrementing count */
+//			_delay_ms(500);
+//		}
+//		serial.puts("Recebendo: ");
+//		twi.Repeated_Start(Slave_Read_Address);	/* Repeated Start I2C communication with SLA+R */
+//		_delay_ms(5);
+//		for (uint8_t i = 0; i < count; i++){
+//			if(i < count - 1){
+//				/* Read and send Acknowledge of data */
+//                info = twi.Read_Ack();
+//                serial.put(info);
+//            }else{
+//                /* Read and Not Acknowledge to data */
+//                info = twi.Read_Nack();
+//                serial.put(info);
+//            }
+//			_delay_ms(500);
+//		}
+//		twi.Stop();							/* Stop I2C */
+//	}
+//    return 0;
+//}
         
         
 //#define Slave_Address			0x20
